5d7fe477f19bacbd75e7cf2cc6473f5811124e40
hesuxiang <you@example.com>
2023-09-14 18:09:29 +0800

    80端口预览vue的html成功

--- app.js
+++ app.js
@@ -1,77 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// 导入 express 模块
+const express = require('express')
+// 创建 express 的服务器实例
+const app = express()
 
-var apiRouter = require('./routes/api');
-var indexRouter = require('./routes/index');
-var settingRouter = require('./routes/setting');
-var loginRouter = require('./routes/login');
-
-//引入
-const JWT = require('./util/JWT');
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const HomeRouter = require("./route/HomeRouter")
+const LoginRouter = require("./route/LoginRouter")
 
 
-//设置中间件，token过期校验
+// 托管静态资源
+app.use(express.static('./dist'))
 
-app.use((req, res, next) => {
-  //排除login相关的路由和接口
-  if (req.url.includes("login")) {
-    next()
-    return
-  }
 
-  const token = req.headers["authorization"]?.split(" ")[1]
-  if (token) {
-    const payload = JWT.verify(token)
-    if (payload) {
-      //重新计算token过期时间
-      const newToken = JWT.generate({
-        _id: payload._id,
-        username: payload.username
-      }, "1d")
-      res.header("Authorization", newToken)
-      next()
-    } else {
-      res.status(401).send({ errCode: -1, errInfo: "token过期" })
-    }
-  } else {
-    next()
-  }
+
+//配置静态资源
+app.use(express.static("public"))
+app.use("/static", express.static("static"))
+
+//应用级别
+app.use("/home", HomeRouter)
+app.use("/login", LoginRouter)
+
+
+app.use((req, res) => {
+  res.status(404).send("丢了")
 })
 
-
-app.use('/', indexRouter);
-app.use('/api', apiRouter);
-app.use('/login', loginRouter);
-app.use('/setting', settingRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+// 调用 app.listen 方法，指定端口号并启动 web 服务器
+const port = 80
+app.listen(port, function () {
+  console.log('Express server running at http://127.0.0.1:' + port)
+})
